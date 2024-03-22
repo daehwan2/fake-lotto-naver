4b4828072d33278a0288d5322cdfe7ec9994ad2e
daehwan2 <dhdh0482@naver.com>
2024-03-22 11:30:14 +0900

    Feat: 네이버 로또 번호 조작하기

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,1 +1,28 @@
 console.log("content-script");
+
+chrome.storage.sync.get(
+  ["number1", "number2", "number3", "number4", "number5", "number6"],
+  (result) => {
+    console.log(result);
+    const number1 = result?.number1;
+    const number2 = result?.number2;
+    const number3 = result?.number3;
+    const number4 = result?.number4;
+    const number5 = result?.number5;
+    const number6 = result?.number6;
+
+    const ballEl1 = document.querySelector(".winning_number .ball.type1");
+    const ballEl2 = document.querySelector(".winning_number .ball.type2");
+    const ballEl3 = document.querySelector(".winning_number .ball.type3");
+    const ballEl4 = document.querySelector(".winning_number .ball.type4");
+    const ballEl5 = document.querySelectorAll(".winning_number .ball.type5")[0];
+    const ballEl6 = document.querySelectorAll(".winning_number .ball.type5")[1];
+
+    if (ballEl1 && number1) ballEl1.innerHTML = number1;
+    if (ballEl2 && number2) ballEl2.innerHTML = number2;
+    if (ballEl3 && number3) ballEl3.innerHTML = number3;
+    if (ballEl4 && number4) ballEl4.innerHTML = number4;
+    if (ballEl5 && number5) ballEl5.innerHTML = number5;
+    if (ballEl6 && number6) ballEl6.innerHTML = number6;
+  }
+);
--- src/popup.ts
+++ src/popup.ts
@@ -1,1 +1,23 @@
 console.log("popup");
+
+const changeBtn = document.querySelector("#change-btn");
+
+changeBtn?.addEventListener("click", () => {
+  const number1 = (document.querySelector("#number1") as HTMLInputElement)
+    .value;
+  const number2 = (document.querySelector("#number2") as HTMLInputElement)
+    .value;
+  const number3 = (document.querySelector("#number3") as HTMLInputElement)
+    .value;
+  const number4 = (document.querySelector("#number4") as HTMLInputElement)
+    .value;
+  const number5 = (document.querySelector("#number5") as HTMLInputElement)
+    .value;
+  const number6 = (document.querySelector("#number6") as HTMLInputElement)
+    .value;
+
+  chrome.storage.sync.set(
+    { number1, number2, number3, number4, number5, number6 },
+    () => {}
+  );
+});
