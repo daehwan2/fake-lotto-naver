5977a2e9f2b53f8ac9de42f3f4526bdc45e50d2f
daehwan2 <dhdh0482@naver.com>
2024-03-22 17:34:09 +0900

    Fix: input type number 스타일 수정 및 저장된 번호로 세팅하기

--- src/popup.ts
+++ src/popup.ts
@@ -2,6 +2,24 @@ console.log("popup");
 
 const changeBtn = document.querySelector("#change-btn");
 
+chrome.storage.sync.get(
+  ["number1", "number2", "number3", "number4", "number5", "number6"],
+  (result) => {
+    const number1 = result?.number1;
+    const number2 = result?.number2;
+    const number3 = result?.number3;
+    const number4 = result?.number4;
+    const number5 = result?.number5;
+    const number6 = result?.number6;
+
+    (document.querySelector("#number1") as HTMLInputElement).value = number1;
+    (document.querySelector("#number2") as HTMLInputElement).value = number2;
+    (document.querySelector("#number3") as HTMLInputElement).value = number3;
+    (document.querySelector("#number4") as HTMLInputElement).value = number4;
+    (document.querySelector("#number5") as HTMLInputElement).value = number5;
+  }
+);
+
 changeBtn?.addEventListener("click", () => {
   const number1 = (document.querySelector("#number1") as HTMLInputElement)
     .value;
